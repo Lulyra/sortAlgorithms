811182d703eab4d986a4eda82b4b819c157d27f7
Luiz Paulo Lyra <luiz@playbookify.io>
2022-09-19 18:34:37 -0300

    feature/config-nav-bar: create and clean the component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { GraphComponent } from './graph/graph.component';
+import { ConfigNavBarComponent } from './config-nav-bar/config-nav-bar.component';
 
 @NgModule({
-  declarations: [AppComponent, GraphComponent],
+  declarations: [AppComponent, GraphComponent, ConfigNavBarComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/config-nav-bar/config-nav-bar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-config-nav-bar',
+  templateUrl: './config-nav-bar.component.html',
+  styleUrls: ['./config-nav-bar.component.scss']
+})
+export class ConfigNavBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
